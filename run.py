3d40b6440fef2089dda77da87be68aafcfff190a
Map9876 <hhdheuwjbgg@outlook.com>
2024-12-28 07:38:08 +0800

    Update run.py

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ c = "curl https://bce.bdstatic.com/p3m/common-service/uploads/wap-article_244e0f
 os.system(c) 
 import cv2
 
-ocr = PaddleOCR(use_gpu=False, lang='en')
+ocr = PaddleOCR(lang='ch')
 img_path = 'wap-article_244e0ff.png'  # 请替换为你的图片路径
 img = cv2.imread(img_path)
 result = ocr.ocr(img, use_gpu=False)
